ef014c300bfd12dbf51bceecfda8798cb3b999f1
maria-magdalena11b <maria-magdalena_bogoeva@abv.bg>
2022-02-10 08:10:11 +0200

    Array

---
+++ Task_0802/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().ToArray();
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                string cmd = command[0];
+                if (cmd=="END")
+                {
+                    break;
+                }
+                switch (cmd)
+                {
+                    case "Reverse":
+                        arr = arr.Reverse().ToArray();
+                        break;
+                    case "Distinct":
+                        arr = arr.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index = int.Parse(command[1]);
+                        string newString = command[2];
+                        if (index>=0&&index<arr.Length)
+                        {
+                            arr[index] = newString;
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalid input");
+                        }
+                    default:
+                        break;
+                        
+                }
+            }
+            Console.WriteLine(string.Join(",",arr));
+        }
+    }
+}
---
+++ Task_0802/Task_0802/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_0802
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+                string cmd = command[0];
+                if (cmd == "Reverse")
+                {
+                    arr = arr.Reverse().ToArray();
+                }
+                else if (cmd=="Distinct")
+                {
+                   arr = arr.Distinct().ToArray();
+                }
+                else if(cmd=="Replace")
+                {
+                    int index = int.Parse(command[1]);
+                    string newString = command[2];
+                    arr[index] = newString;
+                  
+                }
+            
+            }
+            Console.WriteLine(string.Join(",",arr));
+        }
+    }
+}
