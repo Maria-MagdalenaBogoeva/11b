dbd6e2de00c03501e3876bb83ab360d924a0636c
maria-magdalena11b <maria-magdalena_bogoeva@abv.bg>
2022-02-14 08:57:50 +0200

    List

---
+++ List_1402/List_1402/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace List_1402
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
